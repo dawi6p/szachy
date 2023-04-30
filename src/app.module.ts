13109e5fc1bc4945eeec6c2ad870ffc7d4bb2765
unknown <dawi6p@gmail.com>
2023-04-30 15:53:10 +0200

    szachy

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule} from '@nestjs/serve-static';
 import { join } from 'path'; 
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'szachy',
+      entities: [],
+      synchronize: true,
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client/dist'),
     }),
+    AuthModule,
+    UsersModule,
   ],
  controllers: [AppController],
  providers: [AppService],
